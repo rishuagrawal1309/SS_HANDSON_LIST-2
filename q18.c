9bcc98de455927e58bd648538a75a4560d559a63
Rishu Agrawal <rishuagrawal1309@gmail.com>
2025-09-21 11:08:16 +0530

    modified

--- q18.c
+++ q18.c
@@ -7,35 +7,61 @@ execute ls -l | grep ^d | wc ? Use only dup2.
 Date: 20 September, 2025.
 ============================================================================
 */
-#include<unistd.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<fcntl.h>
-#include<sys/wait.h>
-int main()
-{
-   int p1[2];
-   int p2[2];
-   pipe(p1);
-   pipe(p2);
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
-   if(fork()==0)
-   {
-    close(1);
-    dup2(p1[1], STDOUT_FILENO);
-    close(p1[0]); close(p1[1]);
-    close(p2[0]); close(p2[1]);
-    execlp("ls","ls","-l",NULL );
-    perror("execlp ls");
-    exit(1);
+int main() {
+    int pipe1[2], pipe2[2];
 
-   }
+    if (pipe(pipe1) == -1 || pipe(pipe2) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+    if (fork() == 0) {
+        
+        dup2(pipe1[1], STDOUT_FILENO);
+        close(pipe1[0]);
+        close(pipe1[1]);
+        close(pipe2[0]);
+        close(pipe2[1]);
+
+        execlp("ls", "ls", "-l", NULL);
+        perror("execlp ls");
+        exit(1);
+    }
+
    
+    if (fork() == 0) {
+      
+        dup2(pipe1[0], STDIN_FILENO);
+        dup2(pipe2[1], STDOUT_FILENO);
 
-    return 0;
+        close(pipe1[0]);
+        close(pipe1[1]);
+        close(pipe2[0]);
+        close(pipe2[1]);
+
+        execlp("grep", "grep", "^d", NULL);
+        perror("execlp grep");
+        exit(1);
+    }
+
+    dup2(pipe2[0], STDIN_FILENO);
+
+    close(pipe1[0]);
+    close(pipe1[1]);
+    close(pipe2[0]);
+    close(pipe2[1]);
+
+    execlp("wc", "wc", "-l", NULL);
+    perror("execlp wc");
+    exit(1);
 }
+
 /*
 ============================================================================
-
+0
 ============================================================================
 */
